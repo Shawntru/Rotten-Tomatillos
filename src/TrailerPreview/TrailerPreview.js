7aa37151372520bc09fcc0926b95cb89c0dd72bb
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-12-11 11:03:09 -0700

    Add overlay for trailer movie deets

--- src/TrailerPreview/TrailerPreview.js
+++ src/TrailerPreview/TrailerPreview.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactPlayer from 'react-player/youtube';
 import { getMovieVideoData } from '../apiCalls';
 import './TrailerPreview.scss';
 
 class TrailerPreview extends Component {
-  constructor({ trailerInfo }) {
+  constructor() {
     super();
     this.state = {
       trailerInfo: '',
@@ -12,23 +12,41 @@ class TrailerPreview extends Component {
     };
   }
 
-  // TODO: Sad path, divert to generic video if no trailer
+  //737173
+  //this.props.trailerInfo.id
+
   componentDidUpdate = (prevProps) => {
     if (this.props.trailerInfo !== prevProps.trailerInfo) {
       getMovieVideoData(this.props.trailerInfo.id).then((data) =>
         this.setState({
           trailerInfo: data.videos,
-          trailerId: data.videos.find((video) => video.type === 'Trailer').key,
+          trailerId:
+            // '2Gg6Seob5Mg' ||
+            data.videos.find((video) => video.type === 'Trailer').key,
         })
       );
+      if (!this.state.trailerId) this.setState({ trailerId: '2Gg6Seob5Mg' });
     }
   };
 
   render() {
+    const {
+      // backdrop_path,
+      // poster_path,
+      title,
+      release_date,
+      // overview,
+      genres,
+      // budget,
+      // revenue,
+      // tagline,
+      runtime,
+    } = this.props.trailerInfo;
+
     return (
       <section>
-        <div className="player-wrapper">
-          {this.state.trailerId && (
+        {this.state.trailerId && (
+          <div className="player-wrapper">
             <ReactPlayer
               className="react-player"
               // Use of the 'no-cookie' tag prevents share/watch later
@@ -39,22 +57,28 @@ class TrailerPreview extends Component {
               controls={false}
               muted={true}
               playing={true}
+              loop={true}
               config={{
                 youtube: {
                   playerVars: {
                     disablekb: 1,
                     fs: 0,
                     iv_load_policy: 3,
-                    loop: 1,
-                    // playlist: `${props.videoId}`,
+                    playlist: `${this.state.trailerId}`,
                     modestbranding: 1,
                   },
                 },
               }}
             />
-          )}
-        </div>
-        {/* <section className="trailer-info">Place Trailer Info</section> */}
+
+            <section className="trailer-info">
+              <h2 className="trailer-movie-title">{title}</h2>
+              <p className="trailer-details">Released {release_date}</p>
+              <p className="trailer-details">{genres.join(', ')}</p>
+              <p className="trailer-details">{runtime} minutes</p>
+            </section>
+          </div>
+        )}
       </section>
     );
   }
