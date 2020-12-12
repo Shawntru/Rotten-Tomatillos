2eb96f35ec15943a1540000d43594a9518bf83a7
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-12-12 13:15:10 -0700

    Add styling and react player section

--- src/MoviePreview/MoviePreview.js
+++ src/MoviePreview/MoviePreview.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './MoviePreview.scss';
+import ReactPlayer from 'react-player/youtube';
 import { getSingleMovieData } from '../apiCalls';
 import { Link } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ class MoviePreview extends Component {
     super();
     this.state = {
       movieObject: null,
+      movieTrailer: '2Gg6Seob5Mg',
     };
   }
   componentDidMount = () => {
@@ -30,7 +32,11 @@ class MoviePreview extends Component {
 
   render() {
     if (!this.state.movieObject) {
-      return <h3 className="error-message app">We apologize, we couldn't find a match for this movie ID</h3>;
+      return (
+        <h3 className="error-message app">
+          We apologize, we couldn't find a match for this movie ID
+        </h3>
+      );
     }
     const {
       backdrop_path,
@@ -52,31 +58,46 @@ class MoviePreview extends Component {
         className="movie-preview-page "
         style={{ backgroundImage: `url(${backdrop_path})` }}
       >
-        <div className="description-wrapper">
-          <img className="movie-preview-img" src={poster_path} alt={title} />
-          <div className="movie-description">
-            <h3 className="movie-preview-title">{title}</h3>
-            <p className="movie-info-detail">{tagline}</p>
-            <p className="movie-info-detail">
-              Rating: {average_rating.toFixed(1)}
-            </p>
-            <p className="movie-info-detail">Release Date: {release_date}</p>
-            <p className="movie-info-detail">Runtime: {runtime} minutes</p>
-            <p className="movie-info-detail">
-              Budget: {this.switchNumToCurrency(budget)}
-            </p>
-            <p className="movie-info-detail">
-              Revenue: {this.switchNumToCurrency(revenue)}
-            </p>
-            <p className="movie-info-detail">Genre: {genres.join(', ')}</p>
-            <p className="movie-info-detail">Overview: {overview}</p>
-          </div>
-          <Link to="/">
-            <button
-              data-testid="closing-button-element"
-              className="movie-preview-button"
-            ></button>
-          </Link>
+        <div className="preview-wrapper">
+          <div className="description-wrapper">
+            <img className="movie-preview-img" src={poster_path} alt={title} />
+            <div className="movie-description">
+              <h3 className="movie-preview-title">{title}</h3>
+              <p className="movie-info-detail">{tagline}</p>
+              <p className="movie-info-detail">
+                Rating: {average_rating.toFixed(1)}
+              </p>
+              <p className="movie-info-detail">Release Date: {release_date}</p>
+              <p className="movie-info-detail">Runtime: {runtime} minutes</p>
+              <p className="movie-info-detail">
+                Budget: {this.switchNumToCurrency(budget)}
+              </p>
+              <p className="movie-info-detail">
+                Revenue: {this.switchNumToCurrency(revenue)}
+              </p>
+              <p className="movie-info-detail">Genre: {genres.join(', ')}</p>
+              <p className="movie-info-detail">Overview: {overview}</p>
+            </div>
+            <Link to="/">
+              <button
+                data-testid="closing-button-element"
+                className="movie-preview-button"
+              ></button>
+            </Link>
+          </div>{' '}
+          <h3>Trailer</h3>
+          <ReactPlayer
+            className="preview-react-player"
+            url={`https:www.https://www.youtube.com/watch?v=${this.state.movieTrailer}`}
+            controls={true}
+            config={{
+              youtube: {
+                playerVars: {
+                  modestbranding: 1,
+                },
+              },
+            }}
+          />
         </div>
       </section>
     );
